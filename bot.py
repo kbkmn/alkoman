293cfa8bd0a3c4ec8f8e6f85e90be29c34da4919
I.Zhuravlev <freeeg@gmail.com>
2022-03-27 16:07:32 +0300

    emoji added

--- bot.py
+++ bot.py
@@ -130,7 +130,7 @@ def count(update: Update, context: CallbackContext) -> None:
 #     update.inline_query.answer(results)
 
 def debug(update: Update, context: CallbackContext) -> None:
-    resp = "U+1F1FA U+1F1E6 \U0001F1FA \U0001F1E6 \U+1F1FA \U+1F1E6".encode('utf-8')
+    resp = "😑"
 
     update.effective_chat.send_message(resp)
 
